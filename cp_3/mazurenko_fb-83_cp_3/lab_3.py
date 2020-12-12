50eed6b09e16b62365ee4a6fa7cedab253cb1b3e
Viola <violamazurenko@gmail.com>
2020-12-12 02:43:28 +0200

    cp_3_mazurenko_fb-83

--- cp_3/mazurenko_fb-83_cp_3/lab_3.py
+++ cp_3/mazurenko_fb-83_cp_3/lab_3.py
@@ -28,7 +28,7 @@ def linear_comparisons(a, b, m):
         list_of_solutions.append(x)
         return list_of_solutions
     else:
-        if((b % gcd) != 0):
+        if ((b % gcd) != 0):
             return 0
         else:
             a1, b1, m1 = a // gcd, b // gcd, m // gcd
@@ -37,11 +37,16 @@ def linear_comparisons(a, b, m):
             for i in range(1, gcd):
                 list_of_solutions.append(x0 + m1*i)
             return list_of_solutions
-            
+
+#print(linear_comparisons(876, 643, 4234))
+#print(linear_comparisons(10, 44, 943))
+#print(linear_comparisons(1287, 447, 516))
+#print(linear_comparisons(789, 342, 3456))
+ 
 # converting text from file to string
 def convert_text():
 	text = []
-	text_file = open("C:\\Users\\shell\\Desktop\\lab_3\\var.txt", encoding = "utf-8", mode = "r")
+	text_file = open("C:\\Users\\shell\\Desktop\\everything\\lab_3\\var.txt", encoding = "utf-8", mode = "r")
 	text = text_file.read()
 	text_file.close()
 	return text
@@ -95,16 +100,17 @@ def find_top_bigrams():
 		top_bigrams.append(temp[i][0] + temp[i][1])
 	return top_bigrams
 
-def alphabet_humeration():
-	alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
+def alphabet_numeration():
+	alphabet = "абвгдежзийклмнопрстуфхцчшщьыэюя"
 	alphabet_list = []
 	for i in range(len(alphabet)):
 		alphabet_list.append(alphabet[i] + str(i))
 	return alphabet_list
 
+# find the position of the letter
 def alpha_numeration(alpha):
-	alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
-	alphabet_list = alphabet_humeration()
+	alphabet = "абвгдежзийклмнопрстуфхцчшщьыэюя"
+	alphabet_list = alphabet_numeration()
 	for i in range(len(alphabet)):
 		if (alpha == alphabet[i]):
 			if (len(alphabet_list[i]) == 2):
@@ -112,16 +118,39 @@ def alpha_numeration(alpha):
 			elif (len(alphabet_list[i]) == 3):
 				return alphabet_list[i][1] + alphabet_list[i][2]
 
+# find the letter from the known position
+def reverse_alpha_numeration(number):
+	alphabet = "абвгдежзийклмнопрстуфхцчшщьыэюя"
+	alphabet_list = alphabet_numeration()
+	for i in range(len(alphabet)):
+		if ((number < 10) and (len(alphabet_list[i]) == 2)):
+			if (number == int(alphabet_list[i][1])):
+				return alphabet[i][0]
+		elif ((number > 9) and (len(alphabet_list[i]) == 3)):
+			if (number == int(alphabet_list[i][1] + alphabet_list[i][2])):
+				return alphabet[i][0]
+
+# from letters to numbers
 def bigram_numeration(bigram):
 	x1 = int(alpha_numeration(bigram[0]))
 	x2 = int(alpha_numeration(bigram[1]))
 	X = x1 * 31 + x2
 	return X
 
-def check_w():
+# from numbers to letters
+def reverse_bigram_numeration(X):
+	x1 = X // 31
+	x2 = X - x1 * 31
+	X1 = reverse_alpha_numeration(x1)
+	X2 = reverse_alpha_numeration(x2)
+	return X1, X2
+
+
+def find_key():
 	top_rus_bigrams = ["ст", "но", "то", "на", "ен"]
 	top_bigrams = find_top_bigrams()
-	f = open("C:\\Users\\shell\\Desktop\\lab_3\\temp_results.txt", encoding = "utf-8", mode = "w")
+	keys_list = []
+	f = open("C:\\Users\\shell\\Desktop\\everything\\lab_3\\temp_results.txt", encoding = "utf-8", mode = "w")
 	for i in range(5):
 		for k in range(5):
 			y1 = top_bigrams[i]
@@ -141,23 +170,75 @@ def check_w():
 								text_1 = "x1 = " + str(x1) + " x2 = " + str(x2) + '\n'
 								text_2 = "y1 = " + str(y1) + " y2 = " + str(y2) + '\n'
 								text_3 = "X1 = " + str(X1) + " X2 = " + str(X2) + " Y1 = " + str(Y1) + " Y2 = " + str(Y2) + '\n'
-								#print("x1 =", x1, "x2 =", x2)
-								#print("y1 =", y1, "y2 =", y2)
-								#print("X1 =", X1, "X2 =", X2, "Y1 =", Y1, "Y2 =", Y2)
 								for n in range(len(a)):
 									b = (Y1 - a[n] * X1) % pow(31, 2)
 									text_4 = "a = " + str(a[n]) + " b = " + str(b) + '\n'
 									f.write(text_1 + text_2 + text_3 + text_4 + '\n')
-									#print("a =", a[n], "b =", b)
+									keys_list.append(str(a[n]) + " " + str(b))
 									#print('\n')
 	f.close()
-									
-			
-check_w()
-            
-#print(linear_comparisons(5, 12, 19))
-#print(linear_comparisons(1287, 447, 516))
-#print(linear_comparisons(235, -452, 961))
-#print(linear_comparisons(-235, 270, 961))
-#print(linear_comparisons(-249, 270, 961))
-#print(linear_comparisons(377, -452, 961))
+	new_keys_list = list(dict.fromkeys(keys_list))	
+	return new_keys_list				
+
+# take a and b from the chosen element from the list of keys
+def take_key(element):
+	a, b = "", ""
+	i = 0
+	while (element[i] != " "):
+		a = a + element[i]
+		pos = i
+		i += 1
+	for i in range(pos + 2, len(element)):
+		b = b + element[i]
+	return int(a), int(b)
+
+# decode the encrypted text with the given key (a, b)
+def decode_with_key(a, b):
+	bigrams = []
+	bigram_count = 0
+	text = convert_text()
+	decoded_text = ""
+	p = pow(31, 2)
+	for i in range(0, len(text) - 2, 2):
+		bigrams.append(text[i] + text[i + 1])
+		bigram_count += 1
+	for i in range(bigram_count):
+		Y = bigram_numeration(bigrams[i])
+		X = (mod_multi_inverse(a, p)*(Y - b)) % p
+		x1, x2 = reverse_bigram_numeration(X)
+		decoded_text += x1 + x2
+	return decoded_text
+
+# index of conformity for text
+def index_for_text(text):
+	alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
+	n, sum_n = 0, 0
+	for i in range(31):
+		for j in range(len(text)):
+			if (text[j] == alphabet[i]):
+				n += 1
+		if (n > 1):
+			sum_n += n * (n - 1)
+		else:
+			sum_n += 0
+		n = 0
+	index = (1 / (len(text) * (len(text) - 1))) * sum_n
+	return index			
+
+# go through the found keys and find the right one
+def decode_text():
+	keys_list = find_key()
+	f1 = open("C:\\Users\\shell\\Desktop\\everything\\lab_3\\decoded_text.txt", encoding = "utf-8", mode = "w")
+	f2 = open("C:\\Users\\shell\\Desktop\\everything\\lab_3\\keys_results.txt", encoding = "utf-8", mode = "w")
+	for i in range(len(keys_list)):
+		a, b = take_key(keys_list[i])
+		decoded_text = decode_with_key(a, b)
+		key = "(" + str(a) + "," + str(b) + ")"
+		index = "index = " + str(index_for_text(decoded_text))
+		f2.write(key + " " + index + '\n')
+		if (index_for_text(decoded_text) >= 0.05):
+			f1.write(key + '\n' + decoded_text + '\n' + index)
+	f2.close()
+	f1.close()
+
+decode_text()
